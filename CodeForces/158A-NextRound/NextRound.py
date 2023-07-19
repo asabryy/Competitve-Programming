36c9a29168582f554b7d075ca587d185073fa827
asabryy <ahmedsabry1996@hotmail.com>
2023-07-18 21:20:43 -0700

    Next round solution

---
+++ CodeForces/158A-NextRound/NextRound.py
@@ -0,0 +1,26 @@
+def getWinners(n, k, scores):
+    pos = 0
+    kthScore = scores[k-1]
+
+    for i in range(k-1, n-1):
+        if scores[i] < kthScore:
+            pos = i
+            break
+
+    if pos == 0:
+        return n
+    else:
+        return pos
+            
+
+def main():
+    firstLine = input().split(" ")
+    n = int(firstLine[0])
+    k = int(firstLine[1])
+    scores = list(map(int, input().split(" ") ))
+
+    print(getWinners(n, k, scores))
+    
+
+if __name__ == "__main__":
+    main()
