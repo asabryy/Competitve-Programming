cb94c73715c92bfe9248ee1f331e64c6f8a90347
asabryy <ahmedsabry1996@hotmail.com>
2023-07-18 21:43:40 -0700

    Domino Piling solution

---
+++ CodeForces/50A-DominoPiling/DominoPiling.py
@@ -0,0 +1,17 @@
+import math
+
+def getDominos(m, n):
+    return int((m*n)/2)
+
+            
+
+def main():
+    firstLine = input().split(" ")
+    m = int(firstLine[0])
+    n = int(firstLine[1])
+
+    print(getDominos(m, n))
+    
+
+if __name__ == "__main__":
+    main()
